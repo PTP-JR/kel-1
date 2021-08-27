dc86927f76f56f8f561864e1c9751f3dd637adb2
harcengaming <65104722+harcengaming@users.noreply.github.com>
2021-08-27 12:47:07 +0700

    Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-npmimport './App.css';
+import './App.css';
 import About from './components/About';
+import Home from './components/Home';
 
 function App() {
   return (
-    <About />
+    <>
+      <Home />
+      <About />
+    </>
+    
   );
 }
 
