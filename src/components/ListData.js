002a7a38c82523c12b3fd19194fc84ced5c5dee8
aldi-milano <milano.aldi@gmail.com>
2021-08-31 11:08:33 +0700

    add search feature

--- src/components/ListData.js
+++ src/components/ListData.js
@@ -1,17 +1,15 @@
 import { useEffect, useState } from "react";
 import "./ListData.css";
-import './ListData.css';
 
 const GET_API =
   "https://api.themoviedb.org/3/discover/movie?api_key=1d29e799bb3728c10ef98df0f4ed823f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
-
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
-
 const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?api_key=1d29e799bb3728c10ef98df0f4ed823f&language=en-US&page=1&include_adult=false";
+  "https://api.themoviedb.org/3/search/movie?api_key=1d29e799bb3728c10ef98df0f4ed823f&language=en-US&query=";
 
 function ListData() {
   const [movies, setMovies] = useState([]);
+  const [cari, setCari] = useState("");
   useEffect(() => {
     fetch(GET_API)
       .then((response) => response.json())
@@ -22,17 +20,33 @@ function ListData() {
       .catch((err) => console.log(err));
   }, []);
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(SEARCH_API + cari)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.results);
+        console.log(data.results);
+      });
+  };
+
+  const handleOnChange = (e) => {
+    setCari(e.target.value);
+  };
+
   return (
     <>
       <header>
-        <div className="search">
+        <form onSubmit={handleOnSubmit}>
           <input
             type="text"
             placeholder="search"
             className="srch"
-            value={setMovies}
+            value={cari}
+            onChange={handleOnChange}
           />
-        </div>
+        </form>
       </header>
       <div className="movies-container-box">
         {movies.map((item) => (
