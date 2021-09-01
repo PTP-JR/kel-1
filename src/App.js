3e235dfe88a98c46531db29fa74a7d3dc72c1ee7
harcengaming <65104722+harcengaming@users.noreply.github.com>
2021-09-01 10:33:41 +0700

    add Loading...

--- src/App.js
+++ src/App.js
@@ -13,15 +13,15 @@ function App() {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <img src={Logo} className="logo" alt="logo-situs"/>
-        <Navbar.Brand href="#home">The Movies</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-              <Link to="/" className="link-navigasi" style={{ textDecoration: 'none' }}>Home</Link>
-              <Link to="/about" className="link-navigasi" style={{ textDecoration: 'none' }}>About</Link>
-              <Link to="/login" className="link-navigasi login-header" style={{ textDecoration: 'none' }}>Login</Link>
-          </Nav>
-        </Navbar.Collapse>
+          <Navbar.Brand href="#home">The Movies</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+                <Link to="/" className="link-navigasi" style={{ textDecoration: 'none' }}>Home</Link>
+                <Link to="/about" className="link-navigasi" style={{ textDecoration: 'none' }}>About</Link>
+                <Link to="/login" className="link-navigasi login-header" style={{ textDecoration: 'none' }}>Login</Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
 
--- src/components/ListData.js
+++ src/components/ListData.js
@@ -10,12 +10,15 @@ const SEARCH_API =
 function ListData() {
   const [movies, setMovies] = useState([]);
   const [cari, setCari] = useState("");
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch(GET_API)
       .then((response) => response.json())
       .then((data) => {
         setMovies(data.results);
         console.log(data.results);
+        setLoading(false);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -23,11 +26,13 @@ function ListData() {
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
+    setLoading(true)
     fetch(SEARCH_API + cari)
       .then((response) => response.json())
       .then((data) => {
         setMovies(data.results);
         console.log(data.results);
+        setLoading(false);
       });
   };
 
@@ -35,70 +40,80 @@ function ListData() {
     setCari(e.target.value);
   };
 
-  return (
-    <>
+  return ( 
 
-    <section className="video">
-    <div className="video-trailer">
-    <iframe width="560" height="315" className="responsive-iframe" src="https://www.youtube.com/embed/EWizz52lZvw" 
-    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
-    encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-    <section className="deskripsi-video">
-      <h2>The Baby Boss</h2>
-      <h4>Genre: Family</h4>
-     <dix className="social-share">
-       <p>Share This: </p>
-     <a href="#" class="fa fa-facebook"></a>
-     <a href="#" class="fa fa-instagram"></a>
-     <a href="#" class="fa fa-twitter"></a>
-     <a href="#" class="fa fa-linkedin"></a>
-     </dix>
-      <p>Overview: The Templeton brothers — Tim and his Boss Baby little bro Ted — 
-        have become adults and drifted away from each other. But a new boss baby with
-         a cutting-edge approach and a can-do attitude is about to bring them together 
-         again … and inspire a new family business.</p>
+      <>
+        <section className="video">
+            <div className="video-trailer">
+              <iframe 
+                width="560" height="315" className="responsive-iframe" src="https://www.youtube.com/embed/EWizz52lZvw" 
+                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+                encrypted-media; gyroscope; picture-in-picture" allowfullscreen>             
+              </iframe>
+            </div>
 
-    </section>
+          <section className="deskripsi-video">
+            <h2>The Baby Boss</h2>
+            <h4>Genre: Family</h4>
 
+            <dix className="social-share">
+              <p>Share This: </p>
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-instagram"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-linkedin"></a>
+            </dix>
+            <p>
+              Overview: The Templeton brothers — Tim and his Boss Baby little bro Ted — 
+              have become adults and drifted away from each other. But a new boss baby with
+              a cutting-edge approach and a can-do attitude is about to bring them together 
+              again … and inspire a new family business.
+            </p>
+          </section>
+        </section>
 
+      { loading ? <h1 className="loading">Menunggu...</h1> : (
 
-    </section>
-      <section className="fungsi-search">
-        <h1>New Release</h1>
-        <form onSubmit={handleOnSubmit}>
-          <input
-            type="text"
-            placeholder="search"
-            className="srch"
-            value={cari}
-            onChange={handleOnChange}
-          />
-        </form>
-      </section >
-      <div className="movies-container-box">
-        {movies.map((item) => (
-          <div className="movies-container">
-            <img src={IMG_API + item.poster_path} />
-            <div className="movies-info">
-              <div className="movies-name">
-                <div>
-                  <p>{item.title}</p>
-                </div>
-                <div>
-                  <p>{item.vote_average}</p>
-                </div>
+            <>
+              <section className="fungsi-search">
+                <h1>New Release</h1>
+                <form onSubmit={handleOnSubmit}>
+                  <input
+                    type="text"
+                    placeholder="search"
+                    className="srch"
+                    value={cari}
+                    onChange={handleOnChange}
+                  />
+                </form>
+              </section >
+
+              <div className="movies-container-box">
+                {movies.map((item) => (
+                  <div className="movies-container">
+                    <img src={IMG_API + item.poster_path} />
+                      <div className="movies-info">
+                        <div className="movies-name">
+                          <div>
+                            <p>{item.title}</p>
+                          </div>
+                          <div>
+                            <p>{item.vote_average}</p>
+                          </div>
+                        </div>
+                        {/* <p>Release date : {item.release_date}</p> */}
+                      </div>
+                      <div className="movies-overview">
+                        <h3>Overview :</h3>
+                        <p>{item.overview}</p>
+                      </div>
+                  </div>
+                ))}
               </div>
-              {/* <p>Release date : {item.release_date}</p> */}
-            </div>
-            <div className="movies-overview">
-              <h3>Overview :</h3>
-              <p>{item.overview}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
+            </>
+            )
+          } 
+      </>
   );
 }
 
