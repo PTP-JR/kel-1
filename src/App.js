fe96fa4747027ecede442dd02fda7b1936bbf705
molzania <molzania_1507@yahoo.co.id>
2021-09-01 20:08:49 +0700

    commit on 'firsty'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './HeaderFooter/Header';
 import Footer from './HeaderFooter/Header';
+import {Link} from 'react-router-dom';
 import Main from './Main.js';
 import './App.css';
 
@@ -8,7 +9,10 @@ function App() {
     <div className="App">
   <Header/>
   <Main/>
-  <Footer/>
+  <footer>
+        &copy; { new Date().getFullYear() } Copyright  <Link to="https://id.reactjs.org/" rel='noreferrer' target='_blank'> React JS</Link> <br/>
+        Created with Visual Studio Code by Kelompok 1 
+    </footer>
     </div>
   );
 }
