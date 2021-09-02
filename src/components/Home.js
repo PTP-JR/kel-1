3b847665d09e677a9289b35f3faf79caf0833d1d
molzania <molzania_1507@yahoo.co.id>
2021-09-02 14:13:34 +0700

    commit on 'firsty'

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 import NowPlaying from './NowPlaying';
 import './Home.css';
+import videoBackground from '../assets/video1.mp4';
 
 const API_NOWPLAYING = "https://api.themoviedb.org/3/movie/now_playing?api_key=1d29e799bb3728c10ef98df0f4ed823f&language=en-US&page=2"
 
@@ -24,7 +25,17 @@ function Home(){
       <div className="home">
         <section className="section-1">
         <div class="overlay"></div>
-        <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" alt="video-background" src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4"/>
+        <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" 
+        alt="video-background" src={videoBackground}
+        type="video/mp4"/>
+  <div class="container h-100">
+    <div class="d-flex h-100 text-center align-items-center">
+      <div class="w-100 text-white">
+        <h1 class="display-3">Welcome to THE MOVIES</h1>
+        <p class="lead mb-0">Watch Latest Movie inside Here</p>
+      </div>
+    </div>
+  </div>
 
 
 
