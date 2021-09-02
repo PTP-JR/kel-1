ea917311b3125a1a3c91af8578e1be44510fafad
molzania <molzania_1507@yahoo.co.id>
2021-09-02 10:05:43 +0700

    commit on 'firsty'

--- src/components/ListData.js
+++ src/components/ListData.js
@@ -56,10 +56,10 @@ function ListData() {
       <h4>Genre: Family</h4>
      <dix className="social-share">
        <p>Share This: </p>
-     <Link to="#" className="fa fa-facebook"></Link>
-     <Link to="#" className="fa fa-instagram"></Link>
-     <Link to="#" className="fa fa-twitter"></Link>
-     <Link to="#" className="fa fa-linkedin"></Link>
+     <a href="#" class="fa fa-facebook"></a>
+     <a href="#" class="fa fa-instagram"></a>
+     <a href="#" class="fa fa-twitter"></a>
+     <a href="#" class="fa fa-linkedin"></a>
      </dix>
       <p>Overview: The Templeton brothers — Tim and his Boss Baby little bro Ted — 
         have become adults and drifted away from each other. But a new boss baby with
@@ -68,68 +68,49 @@ function ListData() {
           </section>
 </section>
 
-        {loading ? (
-          <img src={loading3D} className="loading" alt="Loading 3D Anime" />
-        ) : (
-          // { loading ? <h1 className="loading">Menunggu...</h1> : (
-            <section className="fungsi-search">
-              <h2>New Release</h2>
-              <form onSubmit={handleOnSubmit}>
-                <input
-                  type="text"
-                  placeholder="search"
-                  className="srch"
-                  value={cari}
-                  onChange={handleOnChange}
-                />
-              </form>
-            </section>
+      { loading ? <img src={loading3D} className="loading" alt="Loading 3D Anime" /> : (
+        // { loading ? <h1 className="loading">Menunggu...</h1> : (
 
+        <>
+              <section className="fungsi-search">
+                <h2>New Release</h2>
+                <form onSubmit={handleOnSubmit}>
+                  <input
+                    type="text"
+                    placeholder="search"
+                    className="srch"
+                    value={cari}
+                    onChange={handleOnChange}
+                  />
+                </form>
+              </section >
 
-
-          { loading ? <img src={loading3D} className="loading" alt="Loading 3D Anime" /> : (
-            // { loading ? <h1 className="loading">Menunggu...</h1> : (
-    <>
-                  <section className="fungsi-search">
-                    <h2>New Release</h2>
-                    <form onSubmit={handleOnSubmit}>
-                      <input
-                        type="text"
-                        placeholder="search"
-                        className="srch"
-                        value={cari}
-                        onChange={handleOnChange}
-                      />
-                    </form>
-                  </section >
-    
-                  <div className="movies-container-box">
-                    {movies.map((item) => (
-                      <div className="movies-container">
-                        <img src={IMG_API + item.poster_path} />
-                          <div className="movies-info">
-                            <div className="movies-name">
-                              <div>
-                                <p>{item.title}</p>
-                              </div>
-                              <div>
-                                <p>{item.vote_average}</p>
-                              </div>
-                            </div>
-                            {/* <p>Release date : {item.release_date}</p> */}
+              <div className="movies-container-box">
+                {movies.map((item) => (
+                  <div className="movies-container">
+                    <img src={IMG_API + item.poster_path} />
+                      <div className="movies-info">
+                        <div className="movies-name">
+                          <div>
+                            <p>{item.title}</p>
                           </div>
-                          <div className="movies-overview">
-                            <h3>Overview :</h3>
-                            <p>{item.overview}</p>
+                          <div>
+                            <p>{item.vote_average}</p>
                           </div>
-                          </div>
+                        </div>
+                        {/* <p>Release date : {item.release_date}</p> */}
                       </div>
-                      </>
-                    ))}
-</div>
+                      <div className="movies-overview">
+                        <h3>Overview :</h3>
+                        <p>{item.overview}</p>
+                      </div>
                   </div>
-                </div>
-
+                ))}
+              </div>
+              </>
+            )}
+            </div>
+      
   );
 }
 
