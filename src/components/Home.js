1a744b5bfcf832e513c5b2a11a2bce77cd9e395e
aldi-milano <milano.aldi@gmail.com>
2021-09-02 11:20:47 +0700

    add Home page and momment package

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
-import react from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import NowPlaying from './NowPlaying';
+
+const API_NOWPLAYING = "https://api.themoviedb.org/3/movie/now_playing?api_key=1d29e799bb3728c10ef98df0f4ed823f&language=en-US&page=2"
 
 function Home(){
+
+    const [playing, setPlaying] = useState([])
+
+    useEffect(() => {
+        fetch(API_NOWPLAYING)
+          .then((response) => response.json())
+          .then((data) => {
+            setPlaying(data.results);
+            console.log(data.results);
+          })
+          .catch((err) => console.log(err));
+      }, []);
+
     return (
-    <>
-    
-    </>
+    <div>
+        <h3>Now Playing: </h3>
+    {playing.map((item) =>  (
+        <NowPlaying {...item}/>
+    ))}
+    </div>
     )
 }
 
